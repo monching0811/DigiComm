4a926aa569a3db0118b2b1051d23e0b8ab14cc77
monching0811 <almondgugulan081100@gmail.com>
2025-04-20 22:44:21 +0800

    update the security for logout

--- logout.php
+++ logout.php
@@ -4,6 +4,8 @@ session_start();
 // Destroy all session data
 session_destroy();
 
+setcookie(session_name(), '', time() - 3600, '/'); // Tanggalin ang session cookie
+
 // Redirect to the login page
 header("Location: login.php");
 exit();
--- permit.php
+++ permit.php
@@ -4,6 +4,7 @@
 session_start();
 
 
+
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
