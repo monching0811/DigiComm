66b41336ab4d0ea3416603a6f126257be923bc3d
monching0811 <almondgugulan081100@gmail.com>
2025-04-21 20:09:36 +0800

    update details

--- accessibility.php
+++ accessibility.php
@@ -10,6 +10,7 @@
     <div class="container">
         <h1>Accessibility</h1>
         <p>This is the Accessibility page content.</p>
+        <h1>hi</h1>
         </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
